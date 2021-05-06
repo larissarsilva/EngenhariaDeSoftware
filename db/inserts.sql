04d89883327574adf0992db8f64f912c2c07d259
Larissa Silva <39226290+larissarsilva@users.noreply.github.com>
2021-05-06 20:37:01 -0300

    Tabela certificado, monitor, candidatura e alterações
    Tabela certificado, monitor, candidatura e alterações nas primary keys e inserts

--- db/inserts.sql
+++ db/inserts.sql
@@ -1,22 +1,23 @@
-INSERT INTO professor (
-codProfessor, 
-email, 
-senha,
-cpf, 
-telefone ) 
-VALUES ( 1, "prof@upe.com.br", "senha123", "01234567899","81997811720");
+INSERT INTO professor (nome, email, senha,cpf, telefone) 
+VALUES ( "Professor","prof@upe.com.br", "senha123", "01234567899","81997811720");
 
-UPDATE Professor SET nome = "Larissa" where codProfessor = 1
+INSERT INTO professor (nome, email, senha,cpf, telefone) 
+VALUES ( "Ester","Ester@upe.com.br", "senha123", "01237577899","81977811720");
 
+INSERT INTO professor (nome, email, senha,cpf, telefone) 
+VALUES ( "Luiz","Luiz@upe.com.br", "senha123", "01235557899","81897811720");
 
-INSERT INTO aluno (
-codAluno,
-nome,
-email,
-senha,
-matricula,
-telefone) VALUES
-(1, "Larissa Silva", "lrpas@ecomp.poli.br", "senha123", "118121300000","8132421680");
+INSERT INTO aluno (nome,email,senha,matricula, telefone) 
+VALUES ("Larissa Silva", "lrpas@ecomp.poli.br", "senha123", "118121300000","8132421680");
+
+INSERT INTO aluno (nome,email,senha,matricula, telefone) 
+VALUES ("Samuel Linkon", "samuel@ecomp.poli.br", "senha3123", "118121300000","8232421680");
+
+INSERT INTO aluno (nome,email,senha,matricula, telefone) 
+VALUES ("Pedro Oliveira", "pedro@ecomp.poli.br", "senhaa123", "218121300500","8532421680");
+
+INSERT INTO aluno (nome,email,senha,matricula, telefone) 
+VALUES ("Joao Vitor", "joaovitor@ecomp.poli.br", "senha7123", "198191300000","8732421680");
 
 INSERT INTO disciplina VALUES ( 1,1, "Engenharia de Software")
 
--- db/sismonitoria.sql
+++ db/sismonitoria.sql
@@ -2,46 +2,63 @@ create database sismonitoria;
 USE sismonitoria;
 
 CREATE TABLE Professor(
-    codProfessor SMALLINT NOT NULL,
+    codProfessor SMALLINT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR (30) NOT NULL,
     email VARCHAR(30) NOT NULL UNIQUE,
     senha VARCHAR(30) NOT NULL,
     cpf VARCHAR(11) NOT NULL UNIQUE,
-    telefone VARCHAR (14) NOT NULL,
-    PRIMARY KEY (codProfessor));
-
+    telefone VARCHAR (14) NOT NULL
+    );
 
 CREATE TABLE Aluno(
-    codAluno SMALLINT NOT NULL,
+    codAluno SMALLINT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR (30) NOT NULL,
     email VARCHAR(30) NOT NULL UNIQUE,
     senha VARCHAR(30) NOT NULL,
     matricula VARCHAR(11) NOT NULL UNIQUE,
     telefone VARCHAR (14) NOT NULL,
-    isMonitor TINYINT NOT NULL
-    PRIMARY KEY (codAluno));
+    isMonitor TINYINT DEFAULT 0
+   );
 
 CREATE TABLE Disciplina(
-    codDisciplina SMALLINT NOT NULL,
+    codDisciplina SMALLINT PRIMARY KEY AUTO_INCREMENT,
     codProfessor SMALLINT NOT NULL,
-    PRIMARY KEY (codDisciplina),
-    FOREIGN KEY (codProfessor) REFERENCES Professor(codProfessor));
-
-
-CREATE TABLE horarioDisciplina(
-    codHorarioDisciplina SMALLINT PRIMARY KEY AUTO_INCREMENT,
     dia VARCHAR (10) NOT NULL,
     horaInicio DATETIME NOT NULL,
-    horaFim DATETIME NOT NULL
+    horaFim DATETIME NOT NULL,
+    FOREIGN KEY (codProfessor) REFERENCES Professor(codProfessor)
+    );
+
+CREATE TABLE Candidatura(
+    codCandidatura SMALLINT PRIMARY KEY AUTO_INCREMENT,
+    codAluno SMALLINT NOT NULL,
+    candidatoAprovado TINYINT,
+    FOREIGN KEY (codAluno) REFERENCES Aluno(codAluno)
+    );
+
+CREATE TABLE Monitor(
+    codMonitor SMALLINT PRIMARY KEY AUTO_INCREMENT,
+    codALuno SMALLINT NOT NULL,
+    FOREIGN KEY (codAluno) REFERENCES Aluno(codAluno)
     );
 
 CREATE TABLE Monitoria(
-    codMonitoria SMALLINT NOT NULL,
+    codMonitoria SMALLINT PRIMARY KEY AUTO_INCREMENT,
     codProfessor SMALLINT NOT NULL,
-    codAluno SMALLINT NOT NULL,
-    codHorarioDisciplina SMALLINT NOT NULL,
-    PRIMARY KEY (codMonitoria),
+    codMonitor SMALLINT NOT NULL,
+    codDisciplina SMALLINT NOT NULL,
+    diaMonitoria VARCHAR (10) NOT NULL,
+    horaInicioMonitoria DATETIME NOT NULL,
+    horaFimMonitoria DATETIME NOT NULL,
     FOREIGN KEY (codProfessor) REFERENCES Professor(codProfessor),
-    FOREIGN KEY (codAluno) REFERENCES Aluno(codAluno),
-    FOREIGN KEY (codHorarioDisciplina) REFERENCES horarioDisciplina(codHorarioDisciplina)
-)    
+    FOREIGN KEY (codMonitor) REFERENCES Monitor(codMonitor),
+    FOREIGN KEY (codDisciplina) REFERENCES Disciplina(codDisciplina)
+    );    
+
+CREATE TABLE Certificado(
+    codCertificado SMALLINT PRIMARY KEY AUTO_INCREMENT,
+    codMonitor SMALLINT NOT NULL,
+    codMonitoria SMALLINT NOT NULL,
+    FOREIGN KEY (codMonitor) REFERENCES Monitor(codMonitor),
+    FOREIGN KEY (codMonitoria) REFERENCES Monitoria(codMonitoria)
+    );
